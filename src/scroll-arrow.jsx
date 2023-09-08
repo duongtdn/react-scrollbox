405ee67b534dbab1872195e6c2fc677e815cfbf9
duongtdn <duongtdn@gmail.com>
2023-09-08 11:34:12 +0700

    add ripple effect

--- src/scroll-arrow.jsx
+++ src/scroll-arrow.jsx
@@ -4,20 +4,28 @@ import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { styled } from 'styled-components';
 
 const icons = {
   up: faCaretUp,
   down: faCaretDown,
 };
 
+const Btn = styled.div`
+  text-align: center;
+  cursor: pointer;
+  transition: opacity 0s;
+  &: active { opacity: 0.5; }
+`
+
 export default function ScrollArrow(props) {
 
   const { direction, onClick } = props;
 
   return (
-    <div style = {{textAlign: 'center', cursor: 'pointer' }} onClick = {onClick}>
+    <Btn onClick = {onClick}>
       <FontAwesomeIcon icon={icons[direction]} />
-    </div>
+    </Btn>
   );
 
 };
